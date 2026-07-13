8e3d7be8749107b207649b6d6a25ef85c9f91b02
Zeki <zekixfly@hotmail.com>
2026-07-13 19:46:25 +0800

    Add a hash check for API_GET_DATA.

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,5 +2,5 @@ Object.defineProperty(window, "API_GET_DATA", {
     configurable: false,
     writable: false,
     enumerable: true,
-    value: (location.href.includes("index") ? location.href.split("index")[0] : location.href) + "jsondata/spatialworks.json",
+    value: (location.href.includes("index") ? location.href.split("index")[0] : location.href.includes("#") ? location.href.split("#")[0] : location.href) + "jsondata/spatialworks.json",
 });
